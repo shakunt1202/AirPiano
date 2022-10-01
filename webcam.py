f63c8a249ed312cc476dc929eb2aa1646da96764
IcHiGo-KuRoSaKiI <vedansh21.2000@gmail.com>
2022-10-01 20:17:12 +0530

    Initial commit

---
+++ webcam.py
@@ -0,0 +1,21 @@
+from threading import Thread
+import cv2
+   
+class Webcam:
+   
+    def __init__(self):
+        # self.video_capture = cv2.VideoCapture(1) ## 2 for droid cam
+        self.video_capture = cv2.VideoCapture(1)
+        self.current_frame = self.video_capture.read()[1]
+           
+    # create thread for capturing images
+    def start(self):
+        Thread(target=self._update_frame, args=()).start()
+   
+    def _update_frame(self):
+        while(True):
+            self.current_frame = self.video_capture.read()[1]
+                   
+    # get the current frame
+    def get_current_frame(self):
+        return self.current_frame
